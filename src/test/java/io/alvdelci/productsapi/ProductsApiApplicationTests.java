e6435e677c2ff50c83c1c7f6b44e35d76a7c102f
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-26 15:14:32 -0300

    initial commit

---
+++ src/test/java/io/alvdelci/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alvdelci.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
