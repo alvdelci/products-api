6d9d761cc7bce5f93b8b36a45da4fd6278664fb9
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-26 17:47:36 -0300

    feat: Adding product crud

--- src/main/java/io/alvdelci/productsapi/controller/ProductController.java
+++ src/main/java/io/alvdelci/productsapi/controller/ProductController.java
@@ -1,17 +1,49 @@
 package io.alvdelci.productsapi.controller;
 
 import io.alvdelci.productsapi.model.Product;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.alvdelci.productsapi.repository.ProductRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("products")
 public class ProductController {
+
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @PostMapping
     public Product save(@RequestBody Product product) {
-        System.out.println("Product saved: " + product);
+        var id = UUID.randomUUID().toString();
+        product.setId(id);
+        productRepository.save(product);
         return product;
+    }
+    @GetMapping("{id}")
+    public Product getById(@PathVariable("id") String id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void remove(@PathVariable("id") String id) {
+        productRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable("id") String id, @RequestBody Product product) {
+        //Quando passamos o id do product o metodo save identifica que o produto ja existe e atualiza as informações. Por isso usamos o mesmo metodo
+        product.setId(id);
+        productRepository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> findAll(@RequestParam("name") String name) {
+        return productRepository.findByName(name);
     }
 }
